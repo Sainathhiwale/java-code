4fb407c16199fdc29a19095c7534578a649b9ca4
Sainathhiwale <sainathhiwale2@gmail.com>
2017-11-07 15:08:14 +0530

    test updated

---
+++ Finbon.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Finbon{
+public static void main(String [] args)
+{
+ int n, first =0, second =1 ,i;
+ int next;
+ System.out.println("enter the any no:");
+ Scanner sc = new Scanner (System.in);
+ n=sc.nextInt();
+ if(n<0)
+{
+  System.out.println("enter the pasitive no please");
+}
+else
+{
+   for(i=0;i<n;i++)
+{
+     System.out.println(first);
+     next = first+second;
+     first = second;
+     second = next;
+}
+}
+}
+}

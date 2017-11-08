efe9a3e22652427f73663253f0e36939aa3d709a
Sainathhiwale <sainathhiwale2@gmail.com>
2017-11-09 00:26:06 +0530

    java code updated

---
+++ java/ReverseNo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+//reverse the number 
+public class ReverseNo{
+	 public static void main(String[] args) {
+
+	 	int num,reverseno=0;
+	 	System.out.println("enter the any no");
+	 	Scanner sc = new Scanner(System.in);
+	 	num = sc.nextInt();
+	 	while(num!=0){
+	 		int sum = num%10;
+	 		reverseno = reverseno *10+sum;
+	 		num=num/10;
+
+	 	}
+	 	System.out.println(reverseno);
+		
+	}
+}

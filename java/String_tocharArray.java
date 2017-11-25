7dd5147cbc25404143bb424778677ec5bd45abc4
Sainathhiwale <sainathhiwale2@gmail.com>
2017-11-25 21:32:13 +0530

    test updated

---
+++ java/String_tocharArray.java
@@ -0,0 +1,16 @@
+package stringbasic;
+
+public class String_tocharArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          String str = new String("sainath hiwale");
+          char[] b = str.toCharArray();
+          for(int i=0;i<b.length;i++){
+        	  System.out.print(b[i]);
+          }
+         
+          
+	}
+
+}

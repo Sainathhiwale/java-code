5936a517cf02b474c51d80af529c292279a0ce57
Sainathhiwale <sainathhiwale2@gmail.com>
2018-06-25 00:01:14 +0530

    javascript data type test updated

---
+++ Javascript/js_data_type.js
@@ -0,0 +1,12 @@
+/* java JavaScript Data Types
+JavaScript variables can hold many data types: numbers, strings, objects and more:
+*/
+
+var length= 16;//Number
+var name ="sainath";//String
+var objAry ={ lastname:"hiwale",address:"pune"};//Object
+
+console.log(length);
+console.log(name);
+console.log(objAry);
+

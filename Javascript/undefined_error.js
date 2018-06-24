0b969d824be3d10ff80f06a5b5e1f22b9ba6d57f
Sainathhiwale <sainathhiwale2@gmail.com>
2018-06-24 23:18:02 +0530

    undefined error test updated

---
+++ Javascript/undefined_error.js
@@ -0,0 +1,22 @@
+
+//undefined error when u dont initialize any value to var varible it's throw error(undefined)
+
+var name;
+console.log(name);
+
+// run the program with help of node js
+// run command : node undefined_error.js 
+
+    // output: undefined
+
+/*
+
+  var name="hello";
+  console.log(name);
+  run the program 
+  node hello.js
+
+output: hello
+
+*/
+

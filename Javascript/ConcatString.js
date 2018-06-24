8ec932d09286e24d11e462ed2a36b15a647c159f
Sainathhiwale <sainathhiwale2@gmail.com>
2018-06-24 23:43:51 +0530

    string concat and undefined error test updated

---
+++ Javascript/ConcatString.js
@@ -0,0 +1,8 @@
+// String concatenate  with + operator
+
+var name ="sainath";
+var surname="hiwale";
+var concat= name+" "+surname;
+console.log(concat);
+
+
--- Javascript/undefined_error.js
+++ Javascript/undefined_error.js
@@ -18,5 +18,7 @@ console.log(name);
 
 output: hello
 
+
+
 */
 

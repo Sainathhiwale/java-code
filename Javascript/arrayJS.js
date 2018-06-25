1af9aa48f0e5f0fe899060244a7cfab3af71be2b
Sainathhiwale <sainathhiwale2@gmail.com>
2018-06-25 14:04:25 +0530

    javascrip- count array length & looping the array element test updated

---
+++ Javascript/arrayJS.js
@@ -0,0 +1,5 @@
+// java script array 
+
+var car = ["bmw", "red" ,"1562" ,"super"]
+var tem = car=[2];
+console.log(tem);
---
+++ Javascript/loopAryJs.js
@@ -0,0 +1,9 @@
+//looping Array Element
+
+var car=["bmw","50000","super","red"];
+var tem=car.length;
+var text="";
+for(i=0;i<tem;i++){
+	  text += "" + car[i] + " ";
+}
+console.log(text);
